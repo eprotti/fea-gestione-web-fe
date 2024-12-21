1287c29d1bbc35254554df6489fcfc1ea9c7d9c8
emiliano.protti.ext@cybersecurity.leonardocompany.com <emiliano.protti.ext@cybersecurity.leonardocompany.com>
2024-12-21 16:43:39 +0100

    f

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userReducer from '../reducers/userReducer.js';
+import userReducer from '../reducers/userReducer';
 import documentsReducer from '../reducers/documentReducer'
 import notificationReducer from '../reducers/notificationReducer'
 import stepsDocumentReducer from '../reducers/stepsDocumentReducer'
